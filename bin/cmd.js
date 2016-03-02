d2cf356d23a2439916e4212fd00cd2bf20da1dba
tsuyoshiwada <tsuyoshi_wada@udstudio.co.jp>
2016-03-02 19:04:42 +0900

    Add cli interface

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,3 +1,44 @@
 #!/usr/bin/env node
 
-console.log("git-diff-archive");
+"use strict";
+
+const fs = require("fs");
+const minimist = require("minimist");
+const colors = require("colors");
+const gitDiffArchive = require("../");
+
+const argv = minimist(process.argv.slice(2), {
+  alias: {
+    F: "diff-filter",
+    f: "format",
+    o: "output",
+    p: "prefix",
+    h: "help"
+  }
+});
+
+// help
+if (argv.help) {
+  fs.createReadStream(__dirname + "/usage.txt").pipe(process.stdout);
+  return;
+}
+
+// parse options
+const commits = argv._.slice();
+const commit = commits[0];
+const oldCommit = commits[1];
+const options = {};
+if (argv["diff-filter"]) options.diffFilter = argv["diff-filter"];
+if (argv.format) options.format = argv.format;
+if (argv.output) options.output = argv.output;
+if (argv.prefix) options.prefix = argv.prefix;
+
+// archive
+gitDiffArchive(commit, oldCommit, options)
+  .then((res) => {
+    console.log(colors.cyan("Done!!"));
+    console.log(colors.cyan("Output >>> "), res.output);
+  })
+  .catch((err) => {
+    console.error(colors.red(err));
+  });
