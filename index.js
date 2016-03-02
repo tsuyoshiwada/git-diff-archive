088be13411c39d9439b120392e213bda7a36f9a2
tsuyoshiwada <tsuyoshi_wada@udstudio.co.jp>
2016-03-02 19:09:26 +0900

    Add base interface

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-"use stric";
+"use strict";
 
-const minimist = require("minimist");
+const path = require("path");
+const fs = require("fs");
+const execSync = require("child_process").execSync;
+const assign = require("object-assign");
+const which = require("which");
+const mkdirp = require("mkdirp");
+const template = require("string-template");
+const Spinner = require("cli-spinner").Spinner;
+const archiver = require("archiver");
 
-// TODO
+const defaults = {
+  diffFilter: "AMCRD",
+  format: "zip",
+  prefix: "files",
+  output: "{dirname}-{datetime}.zip"
+};
+
+
+module.exports = function gitDiffArchive(commit, oldCommit, options) {
+  return new Promise((resolve, reject) => {
+  });
+}
