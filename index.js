206b6e882a113658f6498aa605cd1054c0659f3e
tsuyoshiwada <v23.b.wada@gmail.com>
2016-03-16 03:02:02 +0900

    Update of verbose format

--- index.js
+++ index.js
@@ -147,6 +147,7 @@ function createArchive(files, output, format, prefix, verbose, dryRun) {
 
     const stream = fs.createWriteStream(output);
     const archive = archiver(format);
+    let count = 0;
 
     stream.on("close", () => {
       resolve(archive);
@@ -154,7 +155,8 @@ function createArchive(files, output, format, prefix, verbose, dryRun) {
 
     archive.on("entry", (entry) => {
       if (verbose) {
-        console.log(`${colors.blue.bold("Entried:")} ${entry.name}`);
+        count++;
+        console.log(`${colors.blue.bold(`Entry (${count}/${files.length}):`)} ${entry.name}`);
       }
     });
 
